f192f9b638ab591163f5e4fc0358bb0959e95c25
Ghaus Ali Khan <ghauskhan1616@gmail.com>
2023-10-25 14:13:08 +0500

    create medical_histories table

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -5,3 +5,11 @@ CREATE TABLE patients(
     name VARCHAR (20),
     date_of_birth DATE,
 );
+
+CREATE TABLE medical_histories(
+    id SERIAL PRIMARY KEY,
+    admitted_at TIMESTAMP,
+    patient_id INT,
+    status VARCHAR(100),
+    FOREIGN KEY(patient_id) REFERENCES patients(id)
+);
